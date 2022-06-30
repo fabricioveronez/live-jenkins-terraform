8e118e94254b5aac351e387d477b911bc846bc14
Fabricio Veronez <fabricio@kubedev.io>
2022-06-30 18:47:35 -0300

    Commit inicial

---
+++ src/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.11.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_k8s" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "k8s_cluster" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.resource_k8s.location
+  resource_group_name = azurerm_resource_group.resource_k8s.name
+  dns_prefix          = "labs"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.default_node_pool_count
+    vm_size    = var.default_node_pool_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  
+}
+
+resource "local_file" "kube_config" {
+  content  = azurerm_kubernetes_cluster.k8s_cluster.kube_config_raw
+  filename = "kube_config.yaml"
+}
+
+variable "resource_group_name" {
+  default = "labs-azure"
+}
+
+variable "location" {
+  default = "Brazil South"
+}
+
+variable "cluster_name" {
+  default = "labs-aks"
+}
+
+variable "default_node_pool_count" {
+  default = 2
+}
+
+variable "default_node_pool_size" {
+  default = "Standard_D2_v2"
+}
